84323e7d9522568c985f1f4801c0f4d7e7396083
Domenic Murtari <domenic.murtari@gmail.com>
2014-02-18 10:25:18 -0700

    Functioning input reading

--- huffman.py
+++ huffman.py
@@ -7,8 +7,18 @@ class Huffman():
         self.codebook = {}
         self.encoded = list()
 
+        
+    def readInput(self):
+        for char in self.text:
+            if self.codebook.has_key(char):
+                self.codebook[char] += 1
+            else:
+                self.codebook[char] = 1
     
 def main():
+    huffman = Huffman("hello")
+    huffman.readInput()
+    print huffman.codebook
     
 if __name__ == "__main__":
     main()
