a927fd1373ba3efe8c895f9e36b156aa24d033f4
Domenic Murtari <domenic.murtari@gmail.com>
2014-02-18 09:53:00 -0700

    Added fibonacci by memoization

--- fibonacci.py
+++ fibonacci.py
@@ -10,12 +10,18 @@ class Fibonacci:
             return self.recursive(n - 1) + self.recursive(n - 2)
     
     def memoization(self, n):
-        
+        if n < 2:
+            return n
+        else:
+            if not self.F.has_key(n):
+                self.F[n] = self.memoization(n - 1) + self.memoization(n - 2)
+            return self.F[n]
             
           
 def main():
     fib = Fibonacci()
     print fib.recursive(10)
+    print fib.memoization(10)
   
 if __name__ == "__main__":
     main()
