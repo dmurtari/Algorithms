efe234d0aa16960c1bfdceefbee8a8039f038990
Domenic Murtari <domenic.murtari@gmail.com>
2014-02-18 10:23:03 -0700

    Huffman initialization and basic code structure

---
+++ huffman.py
@@ -0,0 +1,14 @@
+from binaryHeap import BinaryHeap
+
+class Huffman():
+    
+    def __init__(self, text):
+        self.text = text
+        self.codebook = {}
+        self.encoded = list()
+
+    
+def main():
+    
+if __name__ == "__main__":
+    main()
