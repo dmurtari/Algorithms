475717443281d5e69cf48a0c8cac538353f1177f
Domenic Murtari <domenic.murtari@gmail.com>
2014-02-18 09:59:58 -0700

    Added iterative fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,8 @@
 class Fibonacci:
 
     def __init__(self):
-        self.F = {}
+        self.M = {}
+        self.F = list()
         
     def recursive(self, n):
         if n < 2: 
@@ -13,15 +14,23 @@ class Fibonacci:
         if n < 2:
             return n
         else:
-            if not self.F.has_key(n):
-                self.F[n] = self.memoization(n - 1) + self.memoization(n - 2)
-            return self.F[n]
+            if not self.M.has_key(n):
+                self.M[n] = self.memoization(n - 1) + self.memoization(n - 2)
+            return self.M[n]
+            
+    def iteration(self, n):
+        self.F.append(0)
+        self.F.append(1)
+        for i in range(2, n + 1):
+            self.F.append(self.F[i - 1] + self.F[i - 2])
+        return self.F[n]
             
           
 def main():
     fib = Fibonacci()
     print fib.recursive(10)
     print fib.memoization(10)
+    print fib.iteration(10)
   
 if __name__ == "__main__":
     main()
